3e69f2ca112ee71ed31262d411471733b35ed33e
zhongwei wen <zhongweiwen@zhongweideMacBook-Pro.local>
2017-07-26 22:16:50 +0800

    day05

---
+++ www/config_default.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python 
+# -*- coding:utf-8 -*-
+configs = {
+    'db':{
+        'host':'192.168.31.141',
+        'port':3306,
+        'user':'root',
+        'password':'qwer1234',
+        'database':'awesome'
+    },
+    'session':'AwEsOmE'
+}
